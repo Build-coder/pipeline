a05ba9ea14d1beef24f219016ad926d49b1b953e
Build-coder <59242259+Build-coder@users.noreply.github.com>
2021-10-23 11:08:11 -0400

    javalin copy

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -3,26 +3,26 @@ package com.revature;
 import io.javalin.Javalin;
 
 public class Driver {
-
 	public static void main(String[] args) {
-
-		//We first need to create a Javalin object and run the start method to start listening for HTTP requests.
-		//start takes the port number as a param
-		Javalin app = Javalin.create().start(8081);
 		
-		/*
-		 * This .get method will map GET requests to the endpoint that is given as the first Param to be handled
-		 * by the handler. There are methods for any of the HTTP methods/verbs. 
+		/**
+		 * we first need to create a Javalin obj
+		 * run the start method to listn for HTTP req
+		 * the port number as a param
 		 */
-		app.get("/hello", (ctx)->{ctx.result("Hello Javalin! It's good to meet you on /hello "
-				+ "with a GET request");});
+		Javalin app = Javalin.create().start(8080);
 		
-		app.get("/html", ctx->ctx.html("<h1>Hello Javalin! This should be HTML</h1>"));
 		
-		app.post("/hello", (ctx)->{ctx.result("Hello Javalin! It's good to meet you on /hello "
-				+ "with a POST request");
-				ctx.status(203);});
-
+		/**
+		 * This .get method will map GET reqs to the endpt
+		 * that is given as the 1st param to be handled by
+		 * the handler.
+		 * 
+		 * There are methods for any of the HTTP methods/verbs
+		 */
+		app.get("/hello",(ctx)->{ctx.result("<h1>Hello Javalin!"
+				+ "it's good to meet you on /hello a GET req</h1>");});
+		
+		
 	}
-
 }
